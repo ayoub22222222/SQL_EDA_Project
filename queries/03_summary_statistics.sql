15cbe369826a080e3283734c7c427e6b8cce4be6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2025-02-11 17:19:36 +0100

    upadte on notebook

---
+++ queries/03_summary_statistics.sql
@@ -0,0 +1,27 @@
+/*
+employees Data exploration
+
+this file contains queries that will help us to 
+discover our data by displaying the tables and also we will
+summaize our data by calculating the mean median and understand the distribution of the data
+
+Skills used: select,   
+ */
+
+/* use select querie to dipalay tables */
+
+select * from current_dept_emp limit 20;
+
+select * from departments limit 20;
+
+select * from dept_emp limikt 20;
+
+select * from dept_emp_latest_date limit 20;
+
+select * from dept_manager limit 20;
+
+select * from employees limit 20;
+
+select * from salaries limit 20;
+
+select * from titles limit 20;
